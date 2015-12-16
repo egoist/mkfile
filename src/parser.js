521f0fc0a51b5b924dddf9b2dc27ff522ebab5ee
egoist <0x142857@gmail.com>
2015-12-16 11:08:14 +0800

    format

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,5 @@
 const RE_MATCH_TASKS = /(\r?\n){2,}/
 const RE_MATCH_LINES = /\r?\n{1,}/
-const RE_TEST_EXTERNAL_TASK = /a/
 const RE_MATCH_INTRO = /^(\w+)\:\s*([a-zA-Z0-9\.\/]*)/
 
 export default function parser (string) {
@@ -8,12 +7,16 @@ export default function parser (string) {
   for (let [i, s] of string.entries()) {
     let lines = s.split(RE_MATCH_LINES).filter(line => !!line.trim())
     let intro = lines[0]
-    // append `}` to function ending
-    lines.push('}')
-    // match taskName, externalFile from line one
-    // prepend to function heading
     let [, taskName, externalFile] = intro.match(RE_MATCH_INTRO)
-    lines[0] = `export const ${taskName} = () => {`
+    if (externalFile) {
+      // TODO: what if user uses a externalFile as task body?
+    } else {
+      // match taskName, externalFile from line one
+      // prepend to function heading
+      lines[0] = `export const ${taskName} = () => {`
+      // append `}` to function ending
+      lines.push('}')
+    }
     string[i] = lines.join('\n')
   }
   return string.join('\n\n')
